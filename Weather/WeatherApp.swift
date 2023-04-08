798219d761e3b6a48db7b1f524dce618ff16f8eb
221014144 <221014144@students.nust.na>
2023-04-09 00:00:58 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Memory Mhou on 09/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
